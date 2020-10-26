3e37b3427dad19641e0b3bca997e41220c5a2dc4
alexcaughman <alexcaughman7@gmail.com>
2020-10-26 15:12:08 -0400

    formatting changes

--- anychartjs/wordcloud.js
+++ anychartjs/wordcloud.js
@@ -1,20 +1,29 @@
-anychart.onDocumentReady(function() {
+  anychart.onDocumentReady(function() {
     var data = [
       {"x": "Bingo", "value": 3, category: "Positive"},
       {"x": "Night", "value": 2, category: "Negative"},
-      {"x": "Party", "value": 1, category: "Neutral"}
+      {"x": "Party", "value": 1, category: "Neutral"},
+      {"x": "PPPrint", "value": 1, category: "Neutral"},
+      {"x": "Dalmatian", "value": 3, category: "Positive"},
+      {"x": "Daybreak", "value": 2, category: "Negative"},
+      {"x": "Citidel", "value": 1, category: "Neutral"},
+      {"x": "Bogus", "value": 1, category: "Neutral"}
     ];
-  // create a tag (word) cloud chart
+    // set colors
+    var colors = anychart.scales
+      .ordinalColor()
+      .colors(['#7FFF00', '#DC143C', '#B0C4DE']);
+    // create a tag (word) cloud chart
     var chart = anychart.tagCloud(data);
-  // set a chart title
-    chart.title('Five Star vs One Star')
+    // set a chart title
+    chart.title('Postive vs Negative')
     // set an array of angles at which the words will be laid out
-    chart.angles([0])
-    // enable a color range
-    chart.colorRange(true);
+    chart.angles([-45,45])
+    // enable color scale
+    chart.colorScale(colors);
     // set the color range length
     chart.colorRange().length('80%');
-  // display the word cloud chart
+    // display the word cloud chart
     chart.container("container");
     chart.draw();
   });
---
+++ leafletjs/leafconfig.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamltbGVlNDI2MiIsImEiOiJja2V0dXc1MGIwdmpiMndwZW96ZnlqMzl4In0.WFCM5DakYVF0eV_a8C2YDQ";
