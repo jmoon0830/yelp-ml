ab07cecb7da92203732a8e61cbec6d5437e76313
jimlee4262 <jimlee4262@gmail.com>
2020-10-27 20:41:52 -0400

    working on donut

--- chartjs/donut.js
+++ chartjs/donut.js
@@ -1,99 +1,38 @@
-function makeChart(reviews){
-    // rating labels in their own array
-    var ratinglabels = ratings.map(function(d){
-        return d.stars;
-    });
-    // rating score in its own array
-    var ratingsscore = ratings.map(function(d){
-        return +d.score;
-    });
+d3.json("../JSONcopies/file3.json").then(function(data){
+    var data2 = JSON.parse(data) 
+    console.log(data2)
+    console.log(data2.yelp_stars)
 
-    // sort by stars
-    fivestar = [];
-    fourstar = [];
-    threestar = [];
-    twostar = [];
-    onestar = [];
+    onestar = []
+    twostar = []
+    threestar = []
+    fourstar = []
+    fivestar = []
 
-
-    for(var i =0; i<ratings.length;i++){
-        if (ratings[i].stars == "fivestar"){
-            fivestar.push(parseInt(ratings[i].stars))
+    for (var i = 0; data2.yelp_stars.length; i++){
+        if (i == 1){
+            onestar.push(i);
+        } 
+        else if (i == 2){
+            twostar.push(i);
         }
-        else if (ratings[i].stars == "fourstar"){
-            fourstar.push(parseInt(ratings[i].stars))
-        } 
-        else if (ratings[i].stars == "threestar"){
-            threestar.push(parseInt(ratings[i].stars))
-        } 
-        else if (ratings[i].stars == "twostar"){
-            twostar.push(parseInt(ratings[i].stars))
-        } 
-        else if (ratings[i].stars == "onestar"){
-            onestar.push(parseInt(ratings[i].stars))
+        else if (i == 3){
+            threestar.push(i);
         }
-       
+        else if (i == 4){
+            fourstar.push(i);
+        }
+        else {
+            fivestar.push(i);
+        }
     }
-
-
-    // length of star ratings
-    var fivestarlength = fivestar.length
-
-    var fourstarlength = fourstar.length;
-
-    var threestarlength = threestar.length;
-
-    var twostarlength = twostar.length;
-
-    var onestarlength = onestar.length;;
-
-    var starlengthtogether = [fivestarlength, fourstarlength, threestarlength, twostarlength, onestarlength];
-    
-    // generate random ccolor
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF'.split('');
-        var color = '#';
-        for (var i = 0; i < 6; i++ ) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
-    // creating chartjs doughnut chart
-    var chart = new Chart('chart', {
-        type: "doughnut",
-        options:{
-            responsive:true,
-            maintainAspectRatio: true,
-            legend:{
-                display: true
-            },
-            title: {
-                display: true,
-                text: "Pie Chart by Rating",
-                fontSize: 20
-            }
-        },
-        data: {
-            
-            labels: ["fivestar", "fourstar", "threestar", "twostar", "onestar"],
-            datasets:[
-                {
-                    label: "Restaurant Ratings",
-                    data: starlengthtogether,
-                    backgroundColor: ('#FFFFFF')
-                },
-
-            ]
-        }
-    });
-}
-
-
-
+    console.log(onestar)
+    console.log(twostar)
+    console.log(threestar)
+    console.log(fourstar)
+    console.log(fivestar)
+})
 
 
 // https://www.createwithdata.com/chartjs-and-csv/
 
-d3.csv("insert_data_here")
-.then(makeChart);
