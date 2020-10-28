4431f826f3c37cea01d10cfac4e3e66295a2c626
Justin Moon <jmoon0830@gmail.com>
2020-10-27 23:27:53 -0400

    incorporates all parts of the site except deleting previous charts

---
+++ app.py
@@ -0,0 +1,419 @@
+#Import dependencies
+
+#Flask-related dependencies
+from flask import Flask, render_template, jsonify, request
+import json
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+import requests
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+#Web scrape function
+from scrape import scrape
+
+#ML-code-related dependencies
+from textprocess import text_process
+from ml_model import ml_predictor, positive_words, negative_words
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import string
+import operator
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+
+import nltk
+from nltk.corpus import stopwords
+#nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+
+
+
+app = Flask(__name__)
+
+# setup mongo connection
+#conn = "mongodb://localhost:27017"
+#client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+#db = client.yelpDB
+#restaurant = db.yelp
+
+
+#home page
+@app.route("/")
+def index():
+    return render_template("freebie.html")
+
+
+#API Call route
+#This endpoint triggers wiping the recipe DB and sending an API call to populate the DB based on the user's search
+@app.route("/api/home/api_call", methods=['GET', 'POST'])
+def apipull():
+    if request.method == 'POST':  
+        search_params = request.json
+
+        input_name = search_params['name']
+        input_location = search_params['location']
+        print(input_name)
+
+        ##Part 1: Using Yelp Autocomplete to get the restaurant's name
+        #Constructing API query for Yelp API Autocomplete
+        client_id = "Yba0SJQtkua4MCvdfFfUrg"
+        api_key = "xAg1Vq9ZHUJetn_7_V_Q6Y98to85Xq8vIXPesjLSu2DY4TlGtZ15ZdMUdSgHByWrs27p4UtLxik7F24_ga3Cyub545yuL2uXfA4p9supuqkUAY0QXvSkLj_8fCmLX3Yx"
+        headers = {'Authorization': f'Bearer {api_key}'}
+
+        
+        url = "https://api.yelp.com/v3/autocomplete"
+        params = {'text':input_name}
+        response_json = requests.get(url,params=params,headers=headers).json()
+
+        #This is the official name for the restaurant
+        if response_json["terms"]:    
+            first_term = response_json["terms"][0]["text"]
+
+            ##Part 2: Retrieving key info needed, using official name from above to pull the info 
+            url = "https://api.yelp.com/v3/businesses/search"
+            params = {'term':first_term,'location':input_location}
+
+            #Store in JSON
+            response_json2 = requests.get(url,params=params,headers=headers).json()
+
+            # Extracting only the part we need
+            restaurant_info = response_json2['businesses'][0]
+
+            ##Part 3: Retrieving review based on business id
+            business_id = restaurant_info["id"]
+            url = f"https://api.yelp.com/v3/businesses/{business_id}/reviews"
+            params = {'term':first_term,'location':input_location}
+
+            #Store in JSON
+            response_json3 = requests.get(url,params=params,headers=headers).json()
+
+            #Extract Reviews
+            review_info = response_json3["reviews"]
+
+            #Creating Review Objects
+            api_object = {"yelp_api" :restaurant_info,"review_info":review_info}
+            
+            dump_object = dumps(api_object)
+
+            return dump_object
+
+        else:
+            message = "Sorry we don't have that! Try a different search :)"
+            final_message1 = dumps(message)
+            return final_message1
+
+@app.route("/api/home/ml", methods=['GET', 'POST'])
+def ml():
+    if request.method == 'POST':
+        #print(request.data)   
+        search_params = request.json
+        print(search_params)
+
+
+        input_name = search_params['name']
+        input_location = search_params['location']
+
+        print(input_name)
+        print(input_location)
+
+        ##Part 1: Using Yelp Autocomplete to get the restaurant's name
+        #Constructing API query for Yelp API Autocomplete
+        client_id = "Yba0SJQtkua4MCvdfFfUrg"
+        api_key = "xAg1Vq9ZHUJetn_7_V_Q6Y98to85Xq8vIXPesjLSu2DY4TlGtZ15ZdMUdSgHByWrs27p4UtLxik7F24_ga3Cyub545yuL2uXfA4p9supuqkUAY0QXvSkLj_8fCmLX3Yx"
+        headers = {'Authorization': f'Bearer {api_key}'}
+
+        
+        url = "https://api.yelp.com/v3/autocomplete"
+        params = {'text':input_name}
+        response_json = requests.get(url,params=params,headers=headers).json()
+        print(response_json)
+        
+
+        #This is the official name for the restaurant
+    if response_json["terms"]:    
+        first_term = response_json["terms"][0]["text"]
+        print(first_term)
+
+
+        ##Part 2: Retrieving key info needed, using official name from above to pull the info 
+        url = "https://api.yelp.com/v3/businesses/search"
+        params = {'term':first_term,'location':input_location}
+
+        #Store in JSON
+        response_json2 = requests.get(url,params=params,headers=headers).json()
+        #print(response_json2)
+
+        # Extracting only the part we need
+        restaurant_info = response_json2['businesses'][0]
+        #print(restaurant_info)
+        print(type(restaurant_info))
+        print(restaurant_info)
+
+        ## insert JSON into MongoDB
+        #restaurant.insert_one(restaurant_info)
+
+        ##Part 3: web scraping to compile all the reviews
+
+        #Creating URL to be used for web scraping part
+        yelp_restaurant_url = restaurant_info["url"]
+        print(yelp_restaurant_url)
+
+        #Web scraping
+        yelp_scrape_results = scrape(yelp_restaurant_url)
+        #print(yelp_scrape_results)
+        #print(type(yelp_scrape_results))
+
+        #Extracting Yelp Star ratings for later
+        yelp_stars = yelp_scrape_results['Stars']
+
+        #Convert into a dataframe
+        yelp_scrape_df= pd.DataFrame({"Reviews" : yelp_scrape_results['Reviews']})
+        print(yelp_scrape_df)
+
+
+        #Saving to CSV
+        yelp_scrape_df.to_csv("web_scrape_csv", index = False)
+
+
+        #Calling ML functions
+        predicted_reviews = ml_predictor(yelp_scrape_df)
+        #print(predicted_reviews.head())
+
+        #Generative positive reviews for word cloud
+        positive_word_count = positive_words(predicted_reviews)
+
+        #Generating negative reviews for word cloud
+        negative_word_count = negative_words(predicted_reviews)
+
+
+        #Combining objects to send off to JS
+        combined_object = {"yelp_api" :restaurant_info, "positive": positive_word_count, "negative": negative_word_count, "yelp_stars" : yelp_stars}
+        
+
+        #Dumping object
+        final_object = dumps(combined_object)
+
+
+        #Saving as external file to make easier
+        with open('file3.json', 'w') as f:
+            json.dump(final_object, f)
+
+
+        #Sending off to JS
+        return final_object
+
+    else:
+        message = "Uh oh! An error occurred. Please try a different search."
+        final_message = dumps(message)
+        return final_message
+        
+        #return restaurant_info, positive_word_count, negative_word_count
+        #return positive_word_count
+
+
+        #print(type(combined_object))
+        #print(type(final_object))
+
+        #checking
+        #print(final_object)
+        #yelp_api_results = dumps(yelp_api_results)
+
+        #return restaurant_info
+        #return yelp_api_results
+
+
+
+        #exampletuple = positive_word_count
+        #exampletuple = jsonify(exampletuple)
+        #return exampletuple
+
+        #need to create a dictionary object, yelp_json = yelp_json (python dictionary jsonified), ml_results = ml_results
+
+        ##jimmys code, keep in different file, import function, give it the url input, put it inside of 
+        ###research how to send a jsonified python dictionary back to JS 
+
+        #combine yelp_json and ml_results objects together (object of objects), send that back
+       
+    #    combined_object= yelp_json, ml_results
+    #    return combined_object;
+        #return ml_results
+
+
+
+        
+        # background_img = response_json2["businesses"][0]["image_url"]
+        # phone_num = response_json2["businesses"][0]["display_phone"]
+        # address = response_json2["businesses"][0]["location"]["address1"]
+        # is_closed = response_json2["businesses"][0]["is_closed"]
+        # avg_rating = response_json2["businesses"][0]["rating"]
+
+        # print(first_term_url)
+        # print(background_img)
+        # print(phone_num)
+        # print(address)
+        # print(is_closed)
+        # print(avg_rating)
+
+        # #return first_term_url
+
+
+
+        #         ## Pulling what's stored in the JSON from the step above, sending to JS
+        # data = list(restaurant.find())
+        # data2 = list(restaurant.find())
+        
+        # yelp_json = dumps(data)
+        
+        # combined_object = {"object": data + data2}
+
+        # final_object = dumps(combined_object)
+        # print(final_object)
+
+
+
+        # return final_object
+
+       
+
+
+        #first_term_url = 
+
+
+        ###web scrape code in a function, call that function on the yelp url stored in first_term_url
+
+        #feed it into the ML model 
+        #Ml model to parse out postive reviews and negative reviews, store each into a 
+
+
+
+
+        #input_location = 'atlanta, ga'
+
+
+       
+        
+
+       
+        #return result
+        
+
+        # # Establish API Call URL
+        # # drop previously generated collection
+        # restaurant.drop()
+        
+        # # Constructing Query
+        # url = "https://api.edamam.com/search?"
+        # query = f"q={search_param['query']}" #this is where keyword search will go
+        # api_key = f"&app_id={config.api_id}&app_key={config.api_key}"
+        # diet = f"&diet={search_param['diet']}"
+        # calories= f"&calories={search_param['calories']}"
+        # index = f"&from=0&to=100"
+        # query_url = url + query + api_key + diet + calories +index
+        # health_list= search_param['health']
+        # for label in health_list:
+        #     query_url = query_url + f'&health={label}'
+        # try:
+        #     # Make request
+        #     data = requests.get(query_url)
+        #     # Store in JSON
+        #     data_json = data.json()
+        #     recipe_hits = data_json['hits']
+        #     for i in range(len(recipe_hits)):
+        #         recipe = recipe_hits[i]['recipe']
+        #         total_nurtrients = recipe['totalNutrients']
+        #         for key, value in total_nurtrients.items(): 
+        #             if "." in key:
+        #                 new_key = "SUGAR added"
+        #                 old_key = "SUGAR.added"
+        #                 total_nurtrients[new_key] = total_nurtrients.pop(old_key)
+        #     #insert JSON into MongoDB
+        #     recipes.insert_many(recipe_hits)
+
+        # except:
+        # print("error")
+        # data = list(recipes.find())
+        # recipe_json = dumps(data)
+        # return recipe_json
+        # # render an index.html template and pass it the data you retrieved from the database
+    #return render_template("freebie.html", data = results)
+
+# #API Call route
+# #This endpoint triggers wiping the recipe DB and sending an API call to populate the DB based on the user's search
+# @app.route("/api/home/api_call", methods=['GET', 'POST'])
+# def apipull():
+#     if request.method == 'POST':   
+#         search_param = request.json
+#         print(search_param)
+
+#         # Establish API Call URL
+#         # drop previously generated collection
+#         restaurant.drop()
+        
+#         # Constructing Query
+#         url = "https://api.edamam.com/search?"
+#         query = f"q={search_param['query']}" #this is where keyword search will go
+#         api_key = f"&app_id={config.api_id}&app_key={config.api_key}"
+#         diet = f"&diet={search_param['diet']}"
+#         calories= f"&calories={search_param['calories']}"
+#         index = f"&from=0&to=100"
+#         query_url = url + query + api_key + diet + calories +index
+#         health_list= search_param['health']
+#         for label in health_list:
+#             query_url = query_url + f'&health={label}'
+#         try:
+#             # Make request
+#             data = requests.get(query_url)
+#             # Store in JSON
+#             data_json = data.json()
+#             recipe_hits = data_json['hits']
+#             for i in range(len(recipe_hits)):
+#                 recipe = recipe_hits[i]['recipe']
+#                 total_nurtrients = recipe['totalNutrients']
+#                 for key, value in total_nurtrients.items(): 
+#                     if "." in key:
+#                         new_key = "SUGAR added"
+#                         old_key = "SUGAR.added"
+#                         total_nurtrients[new_key] = total_nurtrients.pop(old_key)
+#             #insert JSON into MongoDB
+#             recipes.insert_many(recipe_hits)
+
+#         except:
+#         print("error")
+#         data = list(recipes.find())
+#         recipe_json = dumps(data)
+#         return recipe_json
+#         # # render an index.html template and pass it the data you retrieved from the database
+#         # return render_template("index.html", recipe = data)
+
+
+
+# #route to load the last page based on the recipe id extracted from clicking an image
+# @app.route("/final_recipe/<recipe_id>")
+# def load_recipe(recipe_id):
+#     data = recipes.find_one({"_id" : ObjectId(recipe_id)})
+    
+    
+# # render an index.html template and pass it the data you retrieved from the database
+# return render_template("index2.html", recipe = data)
+
+
+# #brings the final recipe data to JS
+# @app.route("/api/final_recipe/<recipe_id>")
+# def load_recipe2(recipe_id):
+#     print(recipe_id)
+#     data = restaurant.find_one({"_id" : ObjectId(recipe_id)})
+#     print(data)
+#     recipe_json = dumps(data) 
+#     return recipe_json
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
---
+++ static/js/search.js
@@ -0,0 +1,289 @@
+var search_params;
+var button1 = d3.select("#search_button1")
+var profile_img = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png";
+d3.select("#pro_pic1").attr("src",profile_img)
+d3.select("#pro_pic2").attr("src",profile_img)
+d3.select("#pro_pic3").attr("src",profile_img)
+
+// user query is saved and sent to python to webscrape and api call
+button1.on("click", handleChange)
+function handleChange() {
+    $('#loader').show();
+    ///code to turn loader on
+    var input = d3.select("#search_bar1").property("value");
+    var location = d3.select("#search_bar2").property("value");
+    search_params = {
+        "name" : input,
+        "location" : location
+    };
+    console.log(search_params);
+
+    fetch('/api/home/api_call', {
+        method: 'POST',
+        body: JSON.stringify(search_params),
+        headers: new Headers({
+            "content-type": "application/json"
+          })
+      }).then(function (res) {
+          ///console.log(res)
+        return res.json()
+      }).then((data) => {
+        $('#loader').hide();
+          ///code that turns loader off
+        console.log(data);
+        // var pair = data.object[1]
+        // console.log(pair);
+        // var pair1 = data.object[1][0];
+        // var pair2 = data.object[1][1];
+        // console.log(pair1);
+        // console.log(pair2);
+        if (typeof data != "string") {
+            var background_url = data["yelp_api"]["image_url"]
+            console.log(background_url)
+            var auto_name = data["yelp_api"]["name"]
+            console.log(auto_name)
+            var phone_num = data["yelp_api"]["display_phone"];
+            console.log(phone_num)
+            var address_p1 = data["yelp_api"]["location"]["display_address"][0];
+            var address_p2 = data["yelp_api"]["location"]["display_address"][1];
+            console.log(address_p1)
+            console.log(address_p2)
+            var full_address = address_p1 + address_p2
+            console.log(full_address)
+            var is_closed = data["yelp_api"]["is_closed"]
+            console.log(is_closed)
+            var avg_rating = data["yelp_api"]["rating"]
+            console.log(avg_rating)
+            //part one. see below for further note
+            ///it looks like the restaurant name is located in data[0].location.name
+            // changes background img
+            d3.select("#background_img").style("background-image",`url(${background_url})`)
+            // changes current search
+            d3.select("#current_search").select("h2").remove()
+            d3.select("#current_search").append("h2").text(`Current Search: ${auto_name}`)
+            // changes phone number
+            d3.select("#phone_num").select("p").remove()
+            d3.select("#phone_num").append("p").text(phone_num)
+            // changes address
+            d3.select("#address").select("p").remove()
+            d3.select("#address").append("p").text(`${address_p1} ${address_p2}`)
+            // changes open/close
+            d3.select("#hours").select("p").remove()
+            if (is_closed == false) {
+                d3.select("#hours").append("p").text("Open")
+            }
+            else {
+                d3.select("#hours").append("p").text("Closed")
+            }
+            // changes rating
+            d3.select("#rating").select("h3").remove()
+            d3.select("#rating").append("h3").text("Average Rating: " + avg_rating + "/5 Stars")
+            
+            // changes reviewers
+            
+            var reviewer_img1 = data["review_info"][0]["user"]["image_url"]
+            console.log(reviewer_img1)
+            var reviewer_name1 = data["review_info"][0]["user"]["name"]
+            console.log(reviewer_name1)
+            var reviewer_text1 = data["review_info"][0]["text"];
+            console.log(reviewer_text1)
+            var reviewer_rating1 = data["review_info"][0]["rating"];
+            console.log(reviewer_rating1)
+
+            var reviewer_img2 = data["review_info"][1]["user"]["image_url"]
+            var reviewer_name2 = data["review_info"][1]["user"]["name"]
+            var reviewer_text2 = data["review_info"][1]["text"];
+            var reviewer_rating2 = data["review_info"][1]["rating"];
+
+            var reviewer_img3 = data["review_info"][2]["user"]["image_url"]
+            var reviewer_name3 = data["review_info"][2]["user"]["name"]
+            var reviewer_text3 = data["review_info"][2]["text"];
+            var reviewer_rating3 = data["review_info"][2]["rating"];
+
+            var all_reviewer_imgs = [reviewer_img1,reviewer_img2,reviewer_img3];
+            var all_reviewer_names = [reviewer_name1,reviewer_name2,reviewer_name3];
+            var all_reviewer_text = [reviewer_text1,reviewer_text2,reviewer_text3];
+            var all_reviewer_ratings = [reviewer_rating2,reviewer_rating2,reviewer_rating3];
+            var testimonials = ["#testimonial1","#testimonial2","#testimonial3"];
+
+            d3.select("#pro_pic1").attr("src",all_reviewer_imgs[0])
+            d3.select("#pro_pic2").attr("src",all_reviewer_imgs[1])
+            d3.select("#pro_pic3").attr("src",all_reviewer_imgs[2])
+
+            for (var i=0;i<3;i++) {
+
+                d3.select(testimonials[i]).select("h4").remove()
+                d3.select(testimonials[i]).append("h4").text(all_reviewer_names[i])
+
+                d3.select(testimonials[i]).select("p").remove()
+                d3.select(testimonials[i]).append("p").attr("class", "description").text(all_reviewer_ratings[i] + "/5")
+
+                d3.select(testimonials[i]).select("p").remove()
+                d3.select(testimonials[i]).append("p").attr("class", "description").text(all_reviewer_text[i])
+            }
+
+
+        }
+        else {
+        var message = data;
+        // removes all elements
+        d3.select("#background_img").style("background-image","")
+        d3.select("#current_search").select("h2").remove()
+        d3.select("#rating").select("h3").remove()
+        d3.select("#hours").select("p").remove()
+        d3.select("#address").select("p").remove()
+        d3.select("#phone_num").select("p").remove()
+        d3.select("#current_search").append("h2").text(message)
+        }
+
+        var data2 = data;
+        console.log(data2);
+
+        // Create Map, Passing In satelliteMap & earthquakes as Default Layers to Display on Load
+
+        var myMap = L.map("map", {
+          center: [data2.yelp_api.coordinates.latitude, data2.yelp_api.coordinates.longitude],
+          zoom: 15,
+        });
+      
+          // Define variables for our tile layers
+          var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+            tileSize: 512,
+            maxZoom: 18,
+            zoomOffset: -1,
+            id: "mapbox/streets-v11",
+            accessToken: leaflet_key
+          }).addTo(myMap);
+      
+          // Create a new marker
+          // Pass in some initial options, and then add it to the map using the addTo method
+          var marker = L.marker([data2.yelp_api.coordinates.latitude, data2.yelp_api.coordinates.longitude], {
+            draggable: true,
+            title: data2.yelp_api.name
+          }).addTo(myMap);
+      
+          // Binding a pop-up to our marker
+          marker.bindPopup(data2.yelp_api.name);
+
+    })
+
+
+    fetch('/api/home/ml', {
+        method: 'POST',
+        body: JSON.stringify(search_params),
+        headers: new Headers({
+            "content-type": "application/json"
+          })
+      }).then(function (res) {
+          ///console.log(res)
+        return res.json()
+      }).then((data) => {
+          ///code that turns loader off
+        var data2 = data;
+
+        onestar = []
+        twostar = []
+        threestar = []
+        fourstar = []
+        fivestar = []
+    
+        for (var i = 0; i < data2.yelp_stars.length; i++){
+            if (data2.yelp_stars[i] == 1){
+                onestar.push(data2.yelp_stars[i]);
+            } 
+            else if (data2.yelp_stars[i] == 2){
+                twostar.push(data2.yelp_stars[i]);
+            }
+            else if (data2.yelp_stars[i] == 3){
+                threestar.push(data2.yelp_stars[i]);
+            }
+            else if (data2.yelp_stars[i] == 4){
+                fourstar.push(data2.yelp_stars[i]);
+            }
+            else {
+                fivestar.push(data2.yelp_stars[i]);
+            }
+        }
+        console.log(onestar)
+        console.log(twostar)
+        console.log(threestar)
+        console.log(fourstar)
+        console.log(fivestar)
+    
+        star_together = [onestar.length, twostar.length, threestar.length, fourstar.length, fivestar.length]
+    
+        // generate random ccolor
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++ ) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+    
+    
+        var chart = new Chart('chart', {
+            type: "doughnut",
+            options:{
+                responsive:true,
+                maintainAspectRatio: true,
+                aspectRatio:1,
+                legend:{
+                    display: true
+                },
+                title: {
+                    display: true,
+                    text: "Pie Chart by rating",
+                    fontSize: 20
+                }
+            },
+            data: {
+                
+                labels: ["One Star", "Two Star", "Three Star", "Four Star", "Five Star"],
+                datasets:[
+                    {
+                        label: "Yelp Ratings",
+                        data: star_together,
+                        backgroundColor: ["darkred","red","orange","lightgreen","green"]
+                    },
+    
+                ]
+            }
+        });
+
+
+        anychart.onDocumentReady(function() {
+        
+            // create a tag (word) cloud chart
+            var chart = anychart.tagCloud(data2.negative);
+            // set a chart title
+            chart.title('Negative Words')
+            // set an array of angles at which the words will be laid out
+            chart.angles([-45,45])
+            // set the color range length
+            chart.colorRange().length('80%');
+            // display the word cloud chart
+            chart.container("neg");
+            chart.draw();
+        })
+
+        anychart.onDocumentReady(function() {
+          
+            // create a tag (word) cloud chart
+            var chart = anychart.tagCloud(data2.positive);
+            // set a chart title
+            chart.title('Postive Words')
+            // set an array of angles at which the words will be laid out
+            chart.angles([-45,45])
+            // enable color scale
+            chart.colorRange().length('80%');
+            // display the word cloud chart
+            chart.container("pos");
+            chart.draw();
+        })
+
+    })
+
+};
